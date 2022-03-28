d64f11445875b6462ac4222e44199ab3928453be
Devin Spikowski <vegeta897@gmail.com>
2022-03-28 02:08:32 -0400

    :children_crossing: Add camera easing

--- src/camera.ts
+++ src/camera.ts
@@ -1,4 +1,5 @@
 import { PerspectiveCamera, Vector3 } from 'three'
+import { sineOut } from '@gamestdio/easing'
 
 const LEFT = -4
 const RIGHT = 4
@@ -9,8 +10,18 @@ let cameraX = 0
 let cameraY = 0
 
 window.addEventListener('mousemove', (e) => {
-  cameraX = (e.offsetX / window.innerWidth) * (RIGHT - LEFT) + LEFT
-  cameraY = TOP - (e.offsetY / window.innerHeight) * (TOP - BOTTOM)
+  const halfWidth = window.innerWidth / 2
+  if (e.offsetX > halfWidth) {
+    cameraX = sineOut((e.offsetX - halfWidth) / halfWidth) * LEFT
+  } else {
+    cameraX = sineOut(1 - e.offsetX / halfWidth) * RIGHT
+  }
+  const halfHeight = window.innerHeight / 2
+  if (e.offsetY > halfHeight) {
+    cameraY = sineOut((e.offsetY - halfHeight) / halfHeight) * TOP
+  } else {
+    cameraY = sineOut(1 - e.offsetY / halfHeight) * BOTTOM
+  }
 })
 
 const lookAt = new Vector3(0, 0, -10)
