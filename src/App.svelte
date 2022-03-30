3ed400887ed0ed75d476a1f9ad8854ccd6081406
Devin Spikowski <vegeta897@gmail.com>
2022-03-30 03:24:01 -0400

    :children_crossing: Add check for window focus

--- src/App.svelte
+++ src/App.svelte
@@ -5,9 +5,20 @@
 <script lang="ts">
   import { onDestroy, onMount } from 'svelte'
   import HUD from './HUD.svelte'
+
   let threeCanvas: HTMLCanvasElement
+  let hasFocus = false
+
   onMount(() => {
-    initGame(threeCanvas)
+    if (document.hasFocus()) {
+      initGame(threeCanvas)
+      hasFocus = true
+    } else {
+      window.addEventListener('focus', () => {
+        initGame(threeCanvas)
+        hasFocus = true
+      })
+    }
   })
   onDestroy(() => {
     stopGame()
@@ -16,8 +27,21 @@
 
 <main>
   <canvas bind:this={threeCanvas} />
-  <HUD />
+  {#if hasFocus}
+    <HUD />
+  {:else}
+    <h1>Click to focus game</h1>
+  {/if}
 </main>
 
 <style>
+  h1 {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    width: 100%;
+    height: 100vh;
+    color: #c8ff00;
+    font-size: 8vh;
+  }
 </style>
